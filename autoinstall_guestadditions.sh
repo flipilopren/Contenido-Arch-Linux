438e95019d4239f499cf645fa01d379c83a6100d
flipi <132908473+flipilopren@users.noreply.github.com>
2024-05-06 23:27:29 +0200

    Update autoinstall_guestadditions.sh

--- autoinstall_guestadditions.sh
+++ autoinstall_guestadditions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sudo cp -rf /run/media/$USER/VBox_GAs* $HOME
+#Lanzar el script como usuario no privilegiado!!!
 
+sudo cp -rf /run/media/$USER/VBox_GAs* $HOME
 cd $HOME/VBox_GAs*
 sudo chmod 777 VBoxLinuxAdditions.run
 sudo bash VBoxLinuxAdditions.run

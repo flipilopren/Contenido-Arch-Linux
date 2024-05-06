19224f008687ea3f8db82d8af1f81e262c5efe58
flipi <132908473+flipilopren@users.noreply.github.com>
2024-05-06 23:25:56 +0200

    Add files via upload

---
+++ autoinstall_guestadditions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo cp -rf /run/media/$USER/VBox_GAs* $HOME
+
+cd $HOME/VBox_GAs*
+sudo chmod 777 VBoxLinuxAdditions.run
+sudo bash VBoxLinuxAdditions.run
+cd ..
+sudo rm -rf VBox_GAs*

7cd08101026645dff922b5b92295ec702d99b22c
flipi <132908473+flipilopren@users.noreply.github.com>
2024-08-19 04:33:23 +0200

    Add files via upload

---
+++ scripts/script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+read -p "Vas a iniciar un bucle while seguro? Si/No " respuesta
+
+
+
+if [ $respuesta = Si ]
+then
+	respuesta=1
+
+elif [ $respuesta = si ]
+then
+	respuesta=1
+else 
+	echo "No se iniciara el bucle"
+fi
+
+
+
+while [ 1 = $respuesta ]
+
+do	
+	echo "bucle"
+done
